e8e64249584672ceb24f7b2c78f9a971a1d24ea0
Pavel Kryl <kryl@ag028-23.arcig>
2023-10-30 14:28:39 +0100

    typy

--- ucet.py
+++ ucet.py
@@ -2,14 +2,13 @@
 class Ucet:
 
     def __init__(self, zustatek: int =0):
-        self.zustatek = zustatek
+        self.zustatek : float = zustatek
 
-    def pridej(self, kolik) -> None:
+    def pridej(self, kolik: float) -> None:
         self.zustatek += kolik
 
 
 if __name__ == "__main__":
     ucet = Ucet(101)
     ucet.pridej(100)
-    ucet.uber(200)
     print(ucet.zustatek)
